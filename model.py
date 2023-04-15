843d3fb8b9213d6d37458a13e8a438cc518b0330
Simonharris3 <sharris4@oberlin.edu>
2023-04-15 10:09:37 -0400

    bug fixes

--- model.py
+++ model.py
@@ -1,12 +1,11 @@
 import re
-
 import numpy as np
 import tensorflow as tf
 import keras
 import csv
 import random
 
-num_inputs = 9
+num_inputs = 26
 
 
 def main():
@@ -18,60 +17,27 @@ def main():
 
     for row in reader:
         inputs_outputs.append([])
-        for value in row[1:]:
-            inputs_outputs[-1].append(float(preprocess(value)))
+        for i in range(len(row[1:])):
+            value = row[i+1]
+            # for the first value (position), one-hot encode the data
+            inputs_outputs[-1] += preprocess(value, to_categorical=(i == 0))
 
-    random.shuffle(inputs_outputs)
+    # for i in range(len(inputs_outputs)):
+    #     if len(inputs_outputs[i]) != num_inputs+1:
+    #         print(len(inputs_outputs[i]))
+    #         raise ValueError("Expected length of " + str(num_inputs+1) + ", got: " + str(inputs_outputs[i]))
 
-    num_train_data = int(.8 * len(inputs_outputs))  # number of elements that will be in the training data set
-    # first n data points are the training set, rest are the test set
-    train_set = inputs_outputs[:num_train_data]
-    train_input = []
-    train_output = []
-    for player in train_set:
-        # first num_classes-1 values are the input, last value is the output
-        train_input.append(player[:-1])
-        train_output.append(player[-1])
+    train_input, train_output, test_input, test_output = split_train_test(inputs_outputs)
 
-    test_set = inputs_outputs[num_train_data:]
-    test_input = []
-    test_output = []
-    for player in test_set:
-        # first num_classes-1 values are the input, last value is the output
-        test_input.append(player[:-1])
-        test_output.append(player[-1])
-
-    input_layer = keras.Input(shape=num_inputs)
-    l1 = keras.layers.Dense(10, activation="relu")
-    leaky1 = keras.layers.LeakyReLU()
-    l2 = keras.layers.Dense(10, activation="relu")
-    leaky2 = keras.layers.LeakyReLU()
-    output = keras.layers.Dense(1, activation="relu")
-    model = keras.models.Sequential([input_layer, l1, leaky1, l2, leaky2, output])
+    model = leaky_model()
     model.summary()
 
-    model.compile(loss=tf.keras.losses.MeanAbsoluteError(), optimizer=keras.optimizers.Adam(learning_rate=.02))
-    model.fit(train_input, train_output, batch_size=50, epochs=1000)
+    model.compile(loss=tf.keras.losses.MeanAbsoluteError(), optimizer=keras.optimizers.Adam(learning_rate=.01))
+    model.fit(train_input, train_output, batch_size=20, epochs=4000)
     model.evaluate(test_input, test_output)
-    richardson = model.predict(np.array([[0, 76, 244, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]
-
-    negative_counterfactuals = [("position", model.predict(np.array([[3, 76, 244, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
-                                ("height", model.predict(np.array([[0, 71, 244, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
-                                ("weight", model.predict(np.array([[0, 76, 230, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
-                                ("40 time", model.predict(np.array([[0, 76, 244, 4.63, 40.5, 0, 129, 0, 0]]))[0][0]),
-                                ("vert", model.predict(np.array([[0, 76, 244, 4.43, 36.5, 0, 129, 0, 0]]))[0][0]),
-                                ("broad", model.predict(np.array([[0, 76, 244, 4.43, 40.5, 0, 110, 0, 0]]))[0][0])]
-
-    positive_counterfactuals = [("height", model.predict(np.array([[0, 84, 244, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
-                                ("weight", model.predict(np.array([[0, 76, 270, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
-                                ("40 time", model.predict(np.array([[0, 76, 244, 4.23, 40.5, 0, 129, 0, 0]]))[0][0]),
-                                ("vert", model.predict(np.array([[0, 76, 244, 4.43, 44.5, 0, 129, 0, 0]]))[0][0]),
-                                ("bench", model.predict(np.array([[0, 76, 244, 4.43, 40.5, 30, 129, 0, 0]]))[0][0]),
-                                ("broad", model.predict(np.array([[0, 76, 244, 4.43, 40.5, 0, 147, 0, 0]]))[0][0]),
-                                ("3cone", model.predict(np.array([[0, 76, 244, 4.43, 40.5, 0, 129, 7.9, 0]]))[0][0]),
-                                ("shuttle",
-                                 model.predict(np.array([[0, 76, 244, 4.43, 40.5, 0, 129, 6.7, 4.29]]))[0][0])]
-
+    richardson_data = np.array([one_hot(0) + [76, 244, 4.43, 40.5, 0, 129, 0, 0]])
+    richardson = model.predict(richardson_data)[0][0]
+    positive_counterfactuals, negative_counterfactuals = counterfactuals(model)
     print("Anthony Richardson prediction: pick " + str(round(richardson)))
 
     negative_counterfactuals.sort(key=lambda x: x[1])  # sort by the model's prediction
@@ -128,48 +94,47 @@ def main():
         print("strangely, richardson would be worse if his %s was better (pick %.2f)." % (swr[0], swr[1]))
 
 
-def preprocess(value):
-    # create a position code for each position. change to one-hot
+def preprocess(value, to_categorical):
     if value == '':
-        num = '0'
+        rvalue = 0.0
     elif value == 'QB':
-        num = '0'
+        rvalue = 0.0
     elif value == 'OT':
-        num = '1'
+        rvalue = 1.0
     elif value == 'OL' or value == 'OG':
-        num = '2'
+        rvalue = 2.0
     elif value == 'C':
-        num = '3'
+        rvalue = 3.0
     elif value == 'RB' or value == 'FB' or value == 'HB':
-        num = '4'
+        rvalue = 4.0
     elif value == 'TE':
-        num = '5'
+        rvalue = 5.0
     elif value == 'WR':
-        num = '6'
+        rvalue = 6.0
     elif value == 'DT':
-        num = '7'
+        rvalue = 7.0
     elif value == 'DL':
-        num = '8'
+        rvalue = 8.0
     elif value == 'DE' or value == 'EDGE':
-        num = '9'
+        rvalue = 9.0
     elif value == 'OLB':
-        num = '10'
+        rvalue = 10.0
     elif value == 'LB':
-        num = '11'
+        rvalue = 11.0
     elif value == 'CB':
-        num = '12'
+        rvalue = 12.0
     elif value == 'DB':
-        num = '13'
+        rvalue = 13.0
     elif value == 'S':
-        num = '14'
+        rvalue = 14.0
     elif value == 'P':
-        num = '15'
+        rvalue = 15.0
     elif value == 'K':
-        num = '16'
+        rvalue = 16.0
     elif value == 'LS':
-        num = '17'
+        rvalue = 17.0
     elif is_num(value):
-        num = value
+        rvalue = float(value)
     elif '/' in value:
         pick_num = value.split(" / ")
         # find the first letter in the text (should be "rd" or "st" or "nd" or "th")
@@ -178,13 +143,78 @@ def preprocess(value):
         assert (pick_num[2][3:5] == "rd" or pick_num[2][3:5] == "st" or pick_num[2][3:5] == "nd" or
                 pick_num[2][3:5] == "th", "value: " + pick_num[2][3:4])
 
-        num = pick_num[2][:ind]
+        rvalue = float(pick_num[2][:ind])
     else:
         raise Exception("Unknown input: %s" % value)
 
-    return num
+    if to_categorical:
+        rvalue = one_hot(rvalue)
+    else:
+        rvalue = [rvalue]
+
+    # if type(rvalue) == np.ndarray:
+    #     raise Exception("unexpected numpy array: " + str(rvalue))
+
+    return rvalue
 
 
+def split_train_test(data):
+    random.shuffle(data)
+
+    num_train_data = int(.8 * len(data))  # number of elements that will be in the training data set
+    # first n data points are the training set, rest are the test set
+    train_set = data[:num_train_data]
+    train_input = []
+    train_output = []
+    for player in train_set:
+        # first num_classes-1 values are the input, last value is the output
+        train_input.append(player[:-1])
+        train_output.append(player[-1])
+
+    test_set = data[num_train_data:]
+    test_input = []
+    test_output = []
+    for player in test_set:
+        # first num_classes-1 values are the input, last value is the output
+        test_input.append(player[:-1])
+        test_output.append(player[-1])
+
+    return train_input, train_output, test_input, test_output
+
+
+def leaky_model():
+    input_layer = keras.Input(shape=num_inputs)
+    l1 = keras.layers.Dense(10, activation="relu")
+    leaky1 = keras.layers.LeakyReLU()
+    l2 = keras.layers.Dense(10, activation="relu")
+    leaky2 = keras.layers.LeakyReLU()
+    output = keras.layers.Dense(1, activation="relu")
+    return keras.models.Sequential([input_layer, l1, leaky1, l2, leaky2, output])
+
+
+# change the input to the model slightly to see how the result would change
+def counterfactuals(model):
+    return ([("height", model.predict(np.array([one_hot(0)+[84, 244, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
+             ("weight", model.predict(np.array([one_hot(0)+[76, 270, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
+             ("40 time", model.predict(np.array([one_hot(0)+[76, 244, 4.23, 40.5, 0, 129, 0, 0]]))[0][0]),
+             ("vert", model.predict(np.array([one_hot(0)+[76, 244, 4.43, 44.5, 0, 129, 0, 0]]))[0][0]),
+             ("bench", model.predict(np.array([one_hot(0)+[76, 244, 4.43, 40.5, 20, 129, 0, 0]]))[0][0]),
+             ("broad", model.predict(np.array([one_hot(0)+[76, 244, 4.43, 40.5, 0, 147, 0, 0]]))[0][0]),
+             ("3cone", model.predict(np.array([one_hot(0)+[76, 244, 4.43, 40.5, 0, 129, 7.7, 0]]))[0][0]),
+             ("shuttle",
+              model.predict(np.array([one_hot(0)+[76, 244, 4.43, 40.5, 0, 129, 0, 4.5]]))[0][0])],
+            [("position", model.predict(np.array([one_hot(3)+[76, 244, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
+             ("height", model.predict(np.array([one_hot(0)+[71, 244, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
+             ("weight", model.predict(np.array([one_hot(0)+[76, 230, 4.43, 40.5, 0, 129, 0, 0]]))[0][0]),
+             ("40 time", model.predict(np.array([one_hot(0)+[76, 244, 4.63, 40.5, 0, 129, 0, 0]]))[0][0]),
+             ("vert", model.predict(np.array([one_hot(0)+[76, 244, 4.43, 36.5, 0, 129, 0, 0]]))[0][0]),
+             ("broad", model.predict(np.array([one_hot(0)+[76, 244, 4.43, 40.5, 0, 110, 0, 0]]))[0][0])])
+
+
+def one_hot(n):
+    return keras.utils.to_categorical(n, num_classes=18).tolist()
+
+# return whether a string is numeric
 def is_num(value):
     try:
         float(value)
