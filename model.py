607f9f8e62ebf40f2b15378aac05de58898b999c
Simonharris3 <sharris4@oberlin.edu>
2023-05-16 21:16:53 -0400

    more model testing ad tweaks

--- model.py
+++ model.py
@@ -1,13 +1,15 @@
+import copy
 import re
 import numpy as np
 import tensorflow as tf
 import keras
+from keras import initializers, backend
 import csv
 import random
 import math
 
-num_inputs = 46
-epochs = 300
+num_inputs = 38
+epochs = 50
 neurons = [14, 12, 10, 8, 6, 4, 2, 1]
 lr = 0.001
 he_normal = True
@@ -64,20 +66,24 @@ def main():
 
     model = keras.models.load_model("model")
     # layers = [layer for layer in keras.models.load_model("model").layers]
+
     # if he_normal:
     #     initializer = keras.initializers.HeNormal()
     # else:
     #     initializer = keras.initializers.GlorotUniform()
-    #
+
     # layers = []
-    # # # the neurons list maps out how many neurons should be in each layer
+    # # the neurons list maps out how many neurons should be in each layer
     # for i in range(len(neurons)):
     #     layers.append(keras.layers.Dense(neurons[i], activation="relu", kernel_initializer=initializer))
     #     layers.append(keras.layers.LeakyReLU())
 
-    # layers.insert(1, keras.layers.Dense(9, activation="relu", kernel_initializer=initializer, name="new_dense"))
+    # layers.append(keras.layers.Dense(1, activation="relu",
+    #                                  kernel_initializer=initializer, name="new_dense"))
     # layers.insert(1, keras.layers.LeakyReLU(name="new_leaky"))
+
     # model = keras.models.Sequential(layers)
+
     model.build((None, num_inputs))
     model.summary()
 
@@ -86,79 +92,73 @@ def main():
     model.fit(train_input, train_output, epochs=epochs)
     score = model.evaluate(test_input, test_output)
 
-    if score < 5200:
+    if score < 61:
         model.save("model")
         print("model saved!")
 
-    richardson_data = np.array([one_hot(0) + [1, 76, 1, 244, 1, 4.43, 1, 40.5, 0, 0, 1, 129, 0, 0, 0, 0, 767, 80.3, 192,
+    richardson_data = np.array([one_hot(0) + [76, 244, 4.43, 40.5, 0, 129, 0, 0, 767, 80.3, 192,
                                               74.8, 14, 65.5, 0, 0, 0, 0, 0, 0]])
     richardson_prediction = model.predict(richardson_data)[0][0]
-    bad_qb_data = np.array([one_hot(0) + [1, 50, 1, 150, 1, 5, 1, 32, 1, 12, 1, 90, 1, 7.9, 1, 5, 767, 20, 900, 25, 900,
-                                          30, 0, 0, 0, 0, 0, 0]])
+    bad_qb_data = np.array([one_hot(0) + [67, 150, 4.9, 32, 14, 105, 7.7, 4.8, 767, 30, 900, 35, 900,
+                                          40, 0, 0, 0, 0, 0, 0]])
     good_qb_data = np.array(
-        [one_hot(0) + [1, 76, 1, 244, 1, 4.43, 1, 40.5, 0, 0, 1, 129, 1, 6.8, 1, 3.9, 767, 90.3, 700,
-                       87.2, 900, 97.6, 800, 88.5, 900, 99.7, 900, 94.9]])
+        [one_hot(0) + [78, 254, 4.33, 41, 20, 131, 6.8, 3.9, 800, 90.3, 700,
+                       87.2, 900, 97.6, 800, 88.5, 0, 0, 0, 0]])
     bad_qb_prediction = model.predict(bad_qb_data)[0][0]
     good_qb_prediction = model.predict(good_qb_data)[0][0]
-    positive_counterfactuals, negative_counterfactuals = counterfactuals(model, richardson_data)
-    print(positive_counterfactuals)
-    print(negative_counterfactuals)
-    print("Anthony Richardson prediction: pick " + str(round(richardson_prediction)))
-    print("bad qb prediction: pick " + str(round(bad_qb_prediction)))
-    print("good qb prediction: pick " + str(round(good_qb_prediction)))
+    branch_data = np.array([one_hot(position_dict['S']) + [72, 190, 4.58, 34.5, 14, 125, 0, 0, 768, 89.5, 624, 76.6, 290,
+                                          72.4, 0, 0, 0, 0, 0, 0]])
+    branch_prediction = model.predict(branch_data)[0][0]
+    higher_cfs, lower_cfs = counterfactuals(model, richardson_data)
+    print("Anthony Richardson prediction: pick " + str(richardson_prediction))
+    print("bad qb prediction: pick " + str(bad_qb_prediction))
+    print("good qb prediction: pick " + str(good_qb_prediction))
+    print("Brian Branch prediction: pick " + str(branch_prediction) + "\n")
 
-    negative_counterfactuals.sort(key=lambda x: x[1])  # sort by the model's prediction
-    positive_counterfactuals.sort(key=lambda x: x[1])
-    wsr = []
+    lower_cfs.sort(key=lambda x: x[1])  # sort by the model's prediction
+    higher_cfs.sort(key=lambda x: x[1])
+    # worse lower richardson, worse higher richardson, etc
     wlr = []
-    bsr = []
+    whr = []
     blr = []
+    bhr = []
 
-    for i in range(len(negative_counterfactuals)):
-        if negative_counterfactuals[i][1] <= richardson_prediction:
-            if len(bsr) < 4:
-                bsr.append(negative_counterfactuals[i])
-        elif len(wsr) < 4:
-            wsr.append(negative_counterfactuals[i])
+    for i in range(len(lower_cfs)):
+        if lower_cfs[i][1] <= richardson_prediction:
+            blr.append(lower_cfs[i])
+        else:
+            wlr.append(lower_cfs[i])
 
-    while i < len(br):
-        if br[i][1] >= richardson_prediction:
-            br.pop(i)
-            i -= 1
-        i += 1
+    for i in range(len(higher_cfs)):
+        if higher_cfs[i][1] <= richardson_prediction:
+            bhr.append(higher_cfs[i])
+        else:
+            whr.append(higher_cfs[i])
 
-    if len(wr) == 0:
+    # take only the most extreme changes in richardson's pick number
+    wlr = wlr[-3:]
+    whr = whr[-3:]
+    blr = blr[:3]
+    bhr = bhr[:3]
+    if len(wlr) == 0 and len(whr) == 0:
         print("richardson can't get any worse!")
-    if len(wr) == 1:
-        print("richardson would only be worse if his %s was worse (pick %.2f)." % (wr[0][0], wr[0][1]))
-    if len(wr) == 2:
-        print("richardson would be worse if his %s (pick %.2f) or his %s (pick %.2f) was lower." % (wr[0][0], wr[0][1],
-                                                                                                    wr[1][0], wr[1][1]))
-    if len(wr) == 3:
-        print("richardson would be worse if his %s (pick %.2f), his %s (pick %.2f), or his %s (pick %.2f) was lower." %
-              (wr[0][0], wr[0][1], wr[1][0], wr[1][1], wr[2][0], wr[2][1]))
+    else:
+        print("richardson would be worse if the following attributes were higher: ")
+        for i in range(len(whr)):
+            print("%s (pick %.1f)" % (whr[i][0], whr[i][1]))
+        print("\nrichardson would be worse if the following attributes were lower: ")
+        for i in range(len(wlr)):
+            print("%s (pick %.1f)" % (wlr[i][0], wlr[i][1]))
 
-    if len(br) == 0:
-        print("richardson can't get any better!")
-    if len(br) == 1:
-        print("richardson would only be better if his %s was higher (pick %.2f)." % (br[0][0], br[0][1]))
-    if len(br) == 2:
-        print("richardson would be better if his %s (pick %.2f) or his %s (pick %.2f) was higher." %
-              (br[0][0], br[0][1], br[1][0], br[1][1]))
-    if len(br) == 3:
-        print("richardson would be better if his %s (pick %.2f), his %s (pick %.2f), or his %s (pick %.2f) was higher."
-              % (br[0][0], br[0][1], br[1][0], br[1][1], br[2][0], br[2][1]))
-
-    # check if there's a way we could make one of richardson's measurables worse to make the model
-    # like him better, or vice versa
-    if negative_counterfactuals[0][1] < richardson_prediction:
-        # strange_better_richardson is too long
-        sbr = negative_counterfactuals[0]
-        print("richardson would be better if his %s was lower (pick %.2f)." % (sbr[0], sbr[1]))
-
-    if positive_counterfactuals[0][1] > richardson_prediction:
-        swr = positive_counterfactuals[-1]
-        print("richardson would be worse if his %s was higher (pick %.2f)." % (swr[0], swr[1]))
+    if len(blr) == 0 and len(bhr) == 0:
+        print("\nrichardson can't get any better!")
+    else:
+        print("\nrichardson would be better if the following attributes were higher: ")
+        for i in range(len(bhr)):
+            print("%s (pick %.1f)" % (bhr[i][0], bhr[i][1]))
+        print("\nrichardson would be better if the following attributes were lower: ")
+        for i in range(len(blr)):
+            print("%s (pick %.1f)" % (blr[i][0], blr[i][1]))
 
 
 def preprocess(value, to_categorical, is_combine):
@@ -186,13 +186,13 @@ def preprocess(value, to_categorical, is_combine):
 
     if to_categorical:
         rvalue = one_hot(rvalue)
-    elif is_combine:
-        if rvalue == 0:
-            # for the combine data, we insert a data point telling the model whether the player participated in the
-            # combine drill (to potentially improve performance)
-            rvalue = [0, rvalue]
-        else:
-            rvalue = [1, rvalue]
+    # elif is_combine:
+    #     if rvalue == 0:
+    #         # for the combine data, we insert a data point telling the model whether the player participated in the
+    #         # combine drill (to potentially improve performance)
+    #         rvalue = [0, rvalue]
+    #     else:
+    #         rvalue = [1, rvalue]
     else:
         rvalue = [rvalue]
 
@@ -253,16 +253,13 @@ def counterfactuals(model, original):
 
     i = num_classes + 1
     while i < original.size:
-        modified = original
-        modified[0, i] += 10
-        positive_cfs.append((cf_datum(i-num_classes), model.predict(np.array(modified))[0][0]))
-        modified[0, i] -= 20
-        negative_cfs.append((cf_datum(i-num_classes), model.predict(np.array(modified))[0][0]))
+        modified = copy.deepcopy(original)
+        modified[0, i] *= 1.05
+        positive_cfs.append((cf_datum(i - num_classes), model.predict(np.array(modified))[0][0]))
+        modified[0, i] *= .85
+        negative_cfs.append((cf_datum(i - num_classes), model.predict(np.array(modified))[0][0]))
 
-        if i < 33:
-            i += 2
-        else:
-            i += 1
+        i += 1
 
     return positive_cfs, negative_cfs
 
@@ -270,43 +267,43 @@ def counterfactuals(model, original):
 def cf_datum(i):
     if i == 1:
         return "height"
-    if i == 3:
+    if i == 2:
         return "weight"
-    if i == 5:
+    if i == 3:
         return "40 time"
-    if i == 7:
+    if i == 4:
         return "vert"
-    if i == 9:
+    if i == 5:
         return "bench"
-    if i == 11:
+    if i == 6:
         return "broad"
-    if i == 13:
+    if i == 7:
         return "3cone"
-    if i == 15:
+    if i == 8:
         return "shuttle"
-    if i == 16:
+    if i == 9:
         return "2021 snaps"
-    if i == 17:
+    if i == 10:
         return "2021 grade"
-    if i == 18:
+    if i == 11:
         return "2020 snaps"
-    if i == 19:
+    if i == 12:
         return "2020 grade"
-    if i == 20:
+    if i == 13:
         return "2019 snaps"
-    if i == 21:
+    if i == 14:
         return "2019 grade"
-    if i == 22:
+    if i == 15:
         return "2018 snaps"
-    if i == 23:
+    if i == 16:
         return "2018 grade"
-    if i == 24:
+    if i == 17:
         return "2017 snaps"
-    if i == 25:
+    if i == 18:
         return "2017 grade"
-    if i == 26:
+    if i == 19:
         return "2016 snaps"
-    if i == 27:
+    if i == 20:
         return "2016 grade"
     return "idek man"
 
