96f2d76448e8dd273d4330b5fd7e82eacb3e7cf3
Simonharris3 <sharris4@oberlin.edu>
2023-05-18 11:36:50 -0400

    loop through model predictions to find the optimal inputs for certain data points

--- csv_preprocessing.py
+++ csv_preprocessing.py
@@ -1,21 +1,40 @@
 import csv
 
 position_dict = {'QB': 0.0, 'OT': 1.0, 'OL': 2.0, 'OG': 2.0, 'C': 3.0, 'RB': 4.0, 'HB': 4.0, 'FB': 4.0, 'TE': 5.0,
-                 'WR': 6.0, 'DT': 7.0, 'DL': 8.0, 'DE': 9.0, 'EDGE': 9.0, 'OLB': 10.0, 'LB': 11.0, 'CB': 12.0,
+                 'WR': 6.0, 'DT': 7.0, 'DL': 8.0, 'DE': 9.0, 'EDGE': 9.0, 'ED': 9.0, 'OLB': 10.0, 'LB': 11.0, 'CB': 12.0,
                  'DB': 13.0, 'S': 14.0, 'P': 15.0, 'K': 16.0, 'LS': 17.0}
 
 
 def main():
-    # merge_data()
+    merge_data()
     # position_nums_to_letters()
     # test()
-    convert_to_percentile()
+    # convert_to_percentile()
+    # get_rid_of_whitespace()
+
+
+def write_file(file_rep):
+    with open("sportsref_download_with_pff.csv", mode='w', newline='') as file:
+        writer = csv.writer(file)
+        for i in range(len(file_rep)):
+            writer.writerow(file_rep[i])
+
+
+def get_rid_of_whitespace():
+    file_rep = []
+    with open("sportsref_download_with_pff.csv", mode='r') as file:
+        reader = csv.reader(file)
+
+        for row in reader:
+            if len(row) > 0:
+                file_rep.append(row)
+
+    write_file(file_rep)
 
 
 def convert_to_percentile():
     with open("sportsref_download_with_pff.csv", mode='r') as file:
         reader = csv.reader(file)
-        next(reader)
 
         pos_vals = []
         for i in range(12):
@@ -26,15 +45,16 @@ def convert_to_percentile():
         file_rep = []
         for row in reader:
             file_rep.append(row)
-            for i in range(8):
-                pos_num = percentile_pos_num(row[1])
-                try:
-                    datum = float(row[i+2])
-                    if datum != 0:
-                        pos_vals[pos_num][i].append(float(row[i + 2]))
-                except ValueError:
-                    if row[i+2] != '':
-                        raise ValueError("wtf is this: " + row[i+2])
+            if row[0] != "Name":
+                for i in range(8):
+                    pos_num = percentile_pos_num(row[1])
+                    try:
+                        datum = float(row[i+2])
+                        if datum != 0:
+                            pos_vals[pos_num][i].append(float(row[i + 2]))
+                    except ValueError:
+                        if row[i+2] != '':
+                            raise ValueError("wtf is this: " + row[i+2])
 
         for i in range(len(pos_vals)):
             for c in range(len(pos_vals[i])):
@@ -56,10 +76,7 @@ def convert_to_percentile():
                     percentile = -50
                 row[i+2] = percentile
 
-    with open("sportsref_download_with_pff.csv", mode='w') as file:
-        writer = csv.writer(file)
-        for i in range(len(file_rep)):
-            writer.writerow(file_rep[i])
+    write_file(file_rep)
 
 
 def percentile_pos_num(pos):
@@ -131,22 +148,16 @@ def position_nums_to_letters():
 
                 file_rep.append(row_rep)
 
-    with open("sportsref_download_with_pff.csv", mode='w', newline='') as file:
-        writer = csv.writer(file)
-        for i in range(len(file_rep)):
-            writer.writerow(file_rep[i])
+    write_file(file_rep)
 
 
 def merge_data():
     base_file = open("sportsref_download_with_pff.csv", mode='r+')
     reader = csv.reader(base_file)
-    next(reader)
 
     base_data = []
     for row in reader:
-        base_data.append([])
-        for datum in row:
-            base_data[-1].append(datum)
+        base_data.append(row)
 
     base_data = find_matches("defense_summary", base_data)
     base_data = find_matches("passing_summary", base_data)
@@ -154,10 +165,7 @@ def merge_data():
     base_data = find_matches("receiving_summary", base_data)
     base_data = find_matches("offense_blocking", base_data)
 
-    with open("sportsref_download_with_pff.csv", mode='w', newline='') as file:
-        writer = csv.writer(file)
-        for c in range(len(base_data)):
-            writer.writerow(base_data[c])
+    write_file(base_data)
 
 
 # finds players in the pff data that also have combine data, and inserts the pff data into the combine file.
@@ -199,7 +207,6 @@ def find_matches(file_name, base_data):
         for row in reader:
             for base_index in range(len(base_data)):
                 # if the name and position match
-
                 if row[0] == base_data[base_index][0] and same_pos(row[2], base_data[base_index][1]):
                     # find where games played and the grade should be added for the current year
                     output_snaps_index = 11 + 2 * (5 - year_index)
@@ -248,12 +255,10 @@ def find_matches(file_name, base_data):
     for i in range(6):
         files[i].close()
 
-    print(file_name)
     for i in range(len(ex_output)):
         if i < len(ex_output):
             print(ex_output[i])
 
-    print("\n")
     return base_data
 
 
@@ -264,10 +269,12 @@ def same_pos(pos1, pos2):
         (pos2 == 'OL' and (pos1 == 'OT' or pos1 == 'OG' or pos1 == 'C')) or \
         (pos1 == 'RB' and (pos2 == 'HB' or pos2 == 'FB')) or \
         (pos2 == 'RB' and (pos1 == 'HB' or pos1 == 'FB')) or \
-        (pos1 == 'DL' and (pos2 == 'DT' or pos2 == 'EDGE' or pos2 == 'DE' or pos2 == 'DL')) or \
-        (pos2 == 'DL' and (pos1 == 'DT' or pos1 == 'EDGE' or pos1 == 'DE' or pos1 == 'DL')) or \
-        (pos1 == 'EDGE' and (pos2 == 'OLB' or pos2 == 'DE')) or (pos2 == 'EDGE' and (pos1 == 'OLB' or pos1 == 'DE')) \
-        or (pos1 == 'OLB' and pos2 == 'LB') or (pos2 == 'LB' and pos1 == 'OLB') or \
+        (pos1 == 'DL' and (pos2 == 'DT' or pos2 == 'EDGE' or pos2 == 'DE' or pos2 == 'ED' or pos2 == 'DI')) or \
+        (pos2 == 'DL' and (pos1 == 'DT' or pos1 == 'EDGE' or pos1 == 'DE' or pos1 == 'ED' or pos2 == 'DI')) or \
+        (pos1 == 'DI' and pos2 == 'DT') or (pos2 == 'DT' and pos1 == 'DI') or \
+        (pos1 == 'EDGE' and (pos2 == 'OLB' or pos2 == 'DE' or pos2 == 'ED')) or \
+        (pos2 == 'EDGE' and (pos1 == 'OLB' or pos1 == 'DE' or pos1 == 'ED')) or \
+        (pos1 == 'OLB' and pos2 == 'LB') or (pos2 == 'LB' and pos1 == 'OLB') or \
         (pos1 == 'DB' and (pos2 == 'S' or pos2 == 'CB')) or (pos2 == 'DB' and (pos1 == 'S' or pos1 == 'CB'))
 
 
--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import copy
 import re
+import sys
+
 import numpy as np
 import tensorflow as tf
 import keras
@@ -9,9 +11,9 @@ import random
 import math
 
 num_inputs = 40
-epochs = 50
-neurons = [15, 13, 10, 7, 5, 1]
-lr = 0.0015
+epochs = 40
+neurons = [15, 15, 15, 1]
+lr = 0.0003
 train_percentage = .8
 
 he_normal = True
@@ -21,12 +23,12 @@ position_dict = {'QB': 0.0, 'OT': 1.0, 'OL': 2.0, 'OG': 2.0, 'C': 3.0, 'RB': 4.0
 num_positions = 18
 num_combine_data = 8
 row_length = 21
-min_snaps = 200
+min_snaps = 140
 
 load_model = False
-save_model_threshold = 62
+save_model_threshold = 52
 
-cfs_threshold = 0.04
+cfs_threshold = 8
 
 
 def main():
@@ -69,30 +71,37 @@ def main():
     model.fit(train_input, train_output, epochs=epochs)
     score = model.evaluate(test_input, test_output)
 
-    if score < 64:
+    if score < save_model_threshold:
         model.save("model")
         print("model saved!")
 
     # test_cases = []
 
-    richardson_data = ['richardson', 'QB', 76, 244, 4.43, 40.5, 0, 129, 0, 0,
+    richardson_data = ['richardson', 'QB', 73, 98, 1, 100, -50, 100, -50, -50,
                        767, 80.3, 192, 74.8, 14, 65.5, 0, 0, 0, 0, 0, 0, 81]
     # test_cases.append(preprocess_row(richardson_data))
 
-    bad_qb_data = ['bad qb', 'QB', 67, 170, 4.9, 32, 14, 105, 7.7, 4.8,
-                   767, 30, 900, 35, 900, 40, 0, 0, 0, 0, 0, 0, 81]
+    bad_qb_data = ['bad qb', 'QB', 7, 3, 97, 4, 12, 9, 87, 92,
+                   0, 0, 900, 25, 900, 30, 900, 35, 0, 0, 0, 0, 81]
     # test_cases.append(preprocess_row(bad_qb_data))
 
-    good_qb_data = ['good qb', 'QB', 78, 254, 4.33, 41, 20, 131, 6.8, 3.9,
-                    800, 97.3, 700, 95.2, 900, 92.6, 800, 88.5, 0, 0, 0, 0, 81]
+    good_qb_data = ['good qb', 'QB', 96, 100, 6, 100, 98, 99, 0, 2,
+                    0, 0, 700, 95.2, 900, 92.6, 800, 50, 800, 73, 0, 0, 81]
     # test_cases.append(preprocess_row(good_qb_data))
 
-    branch_data = ['branch', 'S', 72, 190, 4.58, 34.5, 14, 125, 0, 0, 768,
-                   89.5, 624, 76.6, 290, 72.4, 0, 0, 0, 0, 0, 0, 81]
+    branch_data = ['branch', 'S', 40, 12, 56, 35, 24, 77, -50, -50,
+                   768, 89.5, 624, 76.6, 290, 72.4, 0, 0, 0, 0, 0, 0, 81]
     # test_cases.append(preprocess_row(branch_data))
 
-    lawrence_data = ['lawrence', 'QB', 77, 213, 0, 0, 0, 0, 0, 0, 0, 0,
-                     624, 91.1, 841, 91.1, 776, 90.7, 0, 0, 0, 0, 81]
+    lawrence_data = ['lawrence', 'QB', 95, 44, -50, -50, -50, -50, -50, -50,
+                     0, 0, 624, 91.1, 841, 91.1, 776, 90.7, 0, 0, 0, 0, 81]
+
+    mid_ed_data = ['mid edge', 'EDGE', 63, 44, 31, 62, 85, 21, 45, 56,
+                   500, 74, 624, 60, 841, 83, 776, 50, 0, 0, 0, 0, 81]
+
+    model_darling_data = ['model darling', 'QB', 1, 1, 1, 1, 50, 12, 1, 1,
+                   500, 1, 624, 1, 841, 99, 776, 1, 500, 1, 500, 99, 81]
+
     # test_cases.append(preprocess_row(lawrence_data))
     #
     # predictions = model.predict(np.asarray(test_cases), verbose=0)
@@ -105,17 +114,19 @@ def main():
     # print("Trevor Lawrence prediction: pick " + str(predictions[4][0]) + '\n')
 
     print()
-    run_counterfactuals(model, richardson_data, cfs_threshold)
-    run_counterfactuals(model, bad_qb_data, cfs_threshold)
-    run_counterfactuals(model, good_qb_data, cfs_threshold)
-    run_counterfactuals(model, branch_data, cfs_threshold)
-    run_counterfactuals(model, lawrence_data, cfs_threshold)
+    # run_counterfactuals(model, richardson_data)
+    run_counterfactuals(model, bad_qb_data)
+    run_counterfactuals(model, good_qb_data)
+    # run_counterfactuals(model, branch_data)
+    # run_counterfactuals(model, lawrence_data)
+    # run_counterfactuals(model, mid_ed_data)
+    run_counterfactuals(model, model_darling_data)
 
 
-def run_counterfactuals(model, data, threshold):
+def run_counterfactuals(model, data):
     player_name = data[0]
-    higher_cfs, lower_cfs, no_data_cfs = counterfactuals(model, np.asarray([preprocess_row(data)]))
-    prediction = model.predict(np.asarray([preprocess_row(data)]), verbose=0)[0][0]
+    higher_cfs, lower_cfs, no_data_cfs, optimals = counterfactuals(model, np.asarray([preprocess_row(data)]))
+    prediction = round(model.predict(np.asarray([preprocess_row(data)]), verbose=0)[0][0], 1)
     print(player_name + " prediction: pick " + str(prediction))
 
     lower_cfs.sort(key=lambda x: x[1])  # sort by the model's prediction
@@ -127,68 +138,113 @@ def run_counterfactuals(model, data, threshold):
     bl_cfs = []
 
     for i in range(len(lower_cfs)):
-        if lower_cfs[i][1] < prediction - threshold*prediction:
+        if lower_cfs[i][1] < prediction - cfs_threshold:
             bl_cfs.append(lower_cfs[i])
-        elif lower_cfs[i][1] > prediction + threshold*prediction:
+        elif lower_cfs[i][1] > prediction + cfs_threshold:
             wl_cfs.append(lower_cfs[i])
 
     wh_cfs = []
     bh_cfs = []
 
     for i in range(len(higher_cfs)):
-        if higher_cfs[i][1] < prediction - threshold*prediction:
+        if higher_cfs[i][1] < prediction - cfs_threshold:
             bh_cfs.append(higher_cfs[i])
-        elif higher_cfs[i][1] > prediction + threshold*prediction:
+        elif higher_cfs[i][1] > prediction + cfs_threshold:
             wh_cfs.append(higher_cfs[i])
 
     wn_cfs = []
     bn_cfs = []
 
     for i in range(len(no_data_cfs)):
-        if no_data_cfs[i][1] < prediction - threshold*prediction:
+        if no_data_cfs[i][1] < prediction - cfs_threshold:
             bn_cfs.append(no_data_cfs[i])
-        elif no_data_cfs[i][1] > prediction + threshold*prediction:
+        elif no_data_cfs[i][1] > prediction + cfs_threshold:
             wn_cfs.append(no_data_cfs[i])
 
-    # take only the most extreme changes in richardson's pick number
-    # wlr = wlr[-3:]
-    # whr = whr[-3:]
-    # blr = blr[:3]
-    # bhr = bhr[:3]
-    # wnr = wnr[-3:]
-    # bnr = bnr[:3]
+    print("optimal stats for %s: " % player_name)
+    for i in range(len(optimals)):
+        print("%s: %.1f (pick %.1f)" % (optimals[i][0], optimals[i][1], optimals[i][2]))
 
-    if len(wh_cfs) != 0:
-        print(player_name + " would be worse if the following attributes were higher: ")
-        for i in range(len(wh_cfs)):
-            print("%s (%.1f -> %.1f, pick %.1f)" % (wh_cfs[i][0], wh_cfs[i][2], wh_cfs[i][3], wh_cfs[i][1]))
+    # if len(wh_cfs) != 0:
+    #     print(player_name + " would be worse if the following attributes were higher: ")
+    #     for i in range(len(wh_cfs)):
+    #         print("%s (%.1f -> %.1f, pick %.1f)" % (wh_cfs[i][0], wh_cfs[i][2], wh_cfs[i][3], wh_cfs[i][1]))
+    #
+    # if len(wl_cfs) != 0:
+    #     print("\n" + player_name + " would be worse if the following attributes were lower: ")
+    #     for i in range(len(wl_cfs)):
+    #         print("%s (%.1f -> %.1f, pick %.1f)" % (wl_cfs[i][0], wl_cfs[i][2], wl_cfs[i][3], wl_cfs[i][1]))
 
-    if len(wl_cfs) != 0:
-        print("\n" + player_name + " would be worse if the following attributes were lower: ")
-        for i in range(len(wl_cfs)):
-            print("%s (%.1f -> %.1f, pick %.1f)" % (wl_cfs[i][0], wl_cfs[i][2], wl_cfs[i][3], wl_cfs[i][1]))
+    # if len(wn_cfs) != 0:
+    #     print("\n" + player_name + " would be worse if there was no data for the following attributes: ")
+    #     for i in range(len(wn_cfs)):
+    #         print("%s (pick %.1f)" % (wn_cfs[i][0], wn_cfs[i][1]))
 
-    if len(wn_cfs) != 0:
-        print("\n" + player_name + " would be worse if there was no data for the following attributes: ")
-        for i in range(len(wn_cfs)):
-            print("%s (pick %.1f)" % (wn_cfs[i][0], wn_cfs[i][1]))
+    # if len(bh_cfs) != 0:
+    #     print("\n" + player_name + " would be better if the following attributes were higher: ")
+    #     for i in range(len(bh_cfs)):
+    #         print("%s (%.1f -> %.1f, pick %.1f)" % (bh_cfs[i][0], bh_cfs[i][2], bh_cfs[i][3], bh_cfs[i][1]))
 
-    if len(bh_cfs) != 0:
-        print("\n" + player_name + " would be better if the following attributes were higher: ")
-        for i in range(len(bh_cfs)):
-            print("%s (%.1f -> %.1f, pick %.1f)" % (bh_cfs[i][0], bh_cfs[i][2], bh_cfs[i][3], bh_cfs[i][1]))
+    # if len(bl_cfs) != 0:
+    #     print("\n" + player_name + " would be better if the following attributes were lower: ")
+    #     for i in range(len(bl_cfs)):
+    #         print("%s (%.1f -> %.1f, pick %.1f)" % (bl_cfs[i][0], bl_cfs[i][2], bl_cfs[i][3], bl_cfs[i][1]))
 
-    if len(bl_cfs) != 0:
-        print("\n" + player_name + " would be better if the following attributes were lower: ")
-        for i in range(len(bl_cfs)):
-            print("%s (%.1f -> %.1f, pick %.1f)" % (bl_cfs[i][0], bl_cfs[i][2], bl_cfs[i][3], bl_cfs[i][1]))
-
-    if len(bn_cfs) != 0:
-        print("\n" + player_name + " would be better if there was no data for the following attributes: ")
-        for i in range(len(bn_cfs)):
-            print("%s (pick %.1f)" % (bn_cfs[i][0], bn_cfs[i][1]))
+    # if len(bn_cfs) != 0:
+    #     print("\n" + player_name + " would be better if there was no data for the following attributes: ")
+    #     for i in range(len(bn_cfs)):
+    #         print("%s (pick %.1f)" % (bn_cfs[i][0], bn_cfs[i][1]))
 
     print("-----------------------------------------------------------------------")
+
+
+# change the input to the model slightly to see how the result would change
+def counterfactuals(model, original):
+    higher_cfs = []
+    lower_cfs = []
+    no_data_cfs = []
+    optimals = []
+
+    original_prediction = model.predict(original, verbose=0)[0][0]
+    i = num_positions + 1
+    while i < original.size:
+        is_combine = i < num_positions + num_combine_data * 2
+        modified = copy.deepcopy(original)
+
+        modified[0, i] += 10
+        higher_cfs.append((cf_datum(i - num_positions), model.predict(modified, verbose=0)[0][0],
+                           original[0, i], modified[0, i]))
+
+        modified[0, i] -= 20
+        lower_cfs.append((cf_datum(i - num_positions), model.predict(modified, verbose=0)[0][0],
+                          original[0, i], modified[0, i]))
+
+        optimal = (sys.maxsize, -81)
+        k = 3
+        while k <= 100:
+            modified[0, i] = k
+            prediction = model.predict(modified, verbose=0)[0][0]
+            if prediction < optimal[0]:
+                optimal = (prediction, k)
+
+            k += 3
+
+        if abs(optimal[0] - original_prediction) > cfs_threshold:
+            optimals.append((cf_datum(i - num_positions), optimal[1], optimal[0]))
+
+        if is_combine:
+            modified[0, i - 1] = 0
+            modified[0, i] = -50
+        else:
+            modified[0, i] = 0
+        no_data_cfs.append((cf_datum(i - num_positions), model.predict(modified, verbose=0)[0][0]))
+
+        if is_combine and not i == num_positions + num_combine_data * 2 - 1:
+            i += 2
+        else:
+            i += 1
+
+    return higher_cfs, lower_cfs, no_data_cfs, optimals
 
 
 def preprocess_row(row):
@@ -241,10 +297,10 @@ def preprocess(value, is_combine, ignore):
             raise Exception("Unknown input: %s" % value)
 
     if is_combine:
-        if rvalue[0] == 0:
+        if rvalue[0] == -50:
             # for the combine data, we insert a data point telling the model whether the player participated in the
             # combine drill (to potentially improve performance)
-            rvalue.insert(0, 0.0)
+            rvalue = [0, 0]
         else:
             rvalue.insert(0, 1.0)
     elif ignore:
@@ -310,32 +366,6 @@ def leaky_model():
     return keras.models.Sequential(layers)
 
 
-# change the input to the model slightly to see how the result would change
-def counterfactuals(model, original):
-    higher_cfs = []
-    lower_cfs = []
-    no_data_cfs = []
-
-    i = num_positions + 1
-    while i < original.size:
-        modified = copy.deepcopy(original)
-        modified[0, i] *= 1.05
-        higher_cfs.append((cf_datum(i - num_positions), model.predict(modified, verbose=0)[0][0],
-                           original[0, i], modified[0, i]))
-
-        modified[0, i] *= .85
-        lower_cfs.append((cf_datum(i - num_positions), model.predict(modified, verbose=0)[0][0],
-                          original[0, i], modified[0, i]))
-
-        modified[0, i-1] = 0
-        modified[0, i] = 0
-        no_data_cfs.append((cf_datum(i - num_positions), model.predict(modified, verbose=0)[0][0]))
-
-        i += 2
-
-    return higher_cfs, lower_cfs, no_data_cfs
-
-
 def cf_datum(i):
     if i == 1:
         return "height"
@@ -353,17 +383,17 @@ def cf_datum(i):
         return "3cone"
     if i == 15:
         return "shuttle"
-    if i == 17:
+    if i == 16:
         return "2021 grade"
-    if i == 19:
+    if i == 17:
         return "2020 grade"
-    if i == 21:
+    if i == 18:
         return "2019 grade"
-    if i == 23:
+    if i == 19:
         return "2018 grade"
-    if i == 25:
+    if i == 20:
         return "2017 grade"
-    if i == 27:
+    if i == 21:
         return "2016 grade"
     raise Exception("counterfactuals bug")
 
